16f83838902ae2c4e77343746f993eb10b88911b
permafrown <bjoelh@gmail.com>
2017-11-16 11:54:43 -0500

    go

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,51 @@ router.get("/about", function(req, res) {
 });
 // END ABOUT ROUTE
 
+// =============================
+//     AUTH ROUTES
+// =============================
+
+// AUTH NEW ROUTE
+router.get("/reg", function(req, res) {
+    res.render("reg", {page: 'reg'});
+});
+// AUTH CREATE ROUTE
+// router.post("/reg", function(req, res) {
+//     var newUser = new User({username: req.body.username});
+//     User.register(newUser, req.body.password, function(err, user) {
+//         if (err) {
+//             console.log(err);
+//             req.flash("error", err.message);
+//             return res.redirect("reg");
+//         }
+//         passport.authenticate("local")(req, res, function() {
+//             req.flash("success", "Successfully registered! Welcome " + user.username);
+//             res.redirect("/campgrounds");
+//         });
+//     });
+// });
+
+// =============================
+//     LOGIN ROUTES
+// =============================
+
+// LOGIN NEW ROUTE
+router.get("/login", function(req, res) {
+    res.render("login", {page: 'login'});
+});
+
+// LOGIN CREATE ROUTE
+// router.post("/login", passport.authenticate("local", {
+//     successRedirect: "/campgrounds",
+//     failureRedirect: "/login"
+//     }), function(req, res) {
+// });
+
+// LOGOUT ROUTE
+// router.get("/logout", function(req, res) {
+//     req.logout();
+//     req.flash("success", "Logged out...");
+//     res.redirect("/campgrounds");
+// });
+
 module.exports = router;
