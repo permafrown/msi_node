cdea45988b00e644f7a36ffeb53f02f2114e319a
permafrown <bjoelh@gmail.com>
2017-11-17 15:04:17 -0500

    go

--- app.js
+++ app.js
@@ -15,6 +15,32 @@ var mysql                   = require("mysql");
 var indexRoutes             = require("./routes/index");
 // END REQUIRING ROUTES
 
+// DB CONFIG
+var connection = mysql.createConnection({
+    host: 'localhost:3306',
+    user: 'bolt_dbU',
+    password: 'eJ4%7y4x',
+    database: 'bolt_DB'
+})
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("the db connection failed");
+    }
+    console.log("the db connection was successful");
+});
+
+// connection.query('SELECT * FROM users', function(err, rows, fields) {
+//     if (err) {
+//         console.log("something broke with the query...");
+//         console.log(err);
+//     }
+//     console.log(rows[0]);
+// });
+
+connection.end();
+// END DB CONFIG
+
 // MODELS
 // END MODELS
 
